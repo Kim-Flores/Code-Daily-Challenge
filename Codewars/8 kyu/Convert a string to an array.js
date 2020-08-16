19399edbcb6f98d4dd0f973c396c6e06f8c4d0d5
Kim Flores <61999639+Kim-Flores@users.noreply.github.com>
2020-08-16 09:33:24 -0400

    Create Convert a string to an array.js

---
+++ Codewars/8 kyu/Convert a string to an array.js
@@ -0,0 +1,18 @@
+
+//Write a function to split a string and convert it into an array of words. For example:
+
+"Robin Singh" ==> ["Robin", "Singh"]
+
+"I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
+
+//solution:
+
+function stringToArray(string){
+	// code code code
+  return string.split(' ');
+
+}
+
+// other solution:
+
+const stringToArray = string => string.split(' ');
