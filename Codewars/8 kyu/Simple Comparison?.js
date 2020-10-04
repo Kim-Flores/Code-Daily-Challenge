514b6797940eb96d9af116788b40a83112161bda
Kim Flores <61999639+Kim-Flores@users.noreply.github.com>
2020-10-03 23:39:35 -0400

    Create Simple Comparison?.js

---
+++ Codewars/8 kyu/Simple Comparison?.js
@@ -0,0 +1,20 @@
+/*
+Instructions:
+Write a function that will compare two values, one will be a number and one will be a string. 
+Return true if they are the same character (regardless of their different data types) and return false if they are not.
+*/
+
+function add(a, b){
+  return a == b ? true : false
+ //longer solution
+//   if (a == b){
+//     return true
+//   }else{
+//     return false
+//   } 
+}
+
+//Other solution:
+function add(a, b){
+  return a == b 
+}
