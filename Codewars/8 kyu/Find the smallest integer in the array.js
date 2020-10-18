ea97e5ef101c2c085e87b2dbb14bb8f53b815a64
Kim Flores <61999639+Kim-Flores@users.noreply.github.com>
2020-10-18 10:36:33 -0400

    Update Find the smallest integer in the array.js

--- Codewars/8 kyu/Find the smallest integer in the array.js
+++ Codewars/8 kyu/Find the smallest integer in the array.js
@@ -9,3 +9,17 @@ You can assume, for the purpose of this kata, that the supplied array will not b
 */
 
 //Solution:
+class SmallestIntegerFinder {
+  findSmallestInt(args) {
+    let sortedArr = args.sort((a,b) => (a-b))
+      return sortedArr[0]
+    
+  }
+}
+
+//Other Solution:
+class SmallestIntegerFinder {
+  findSmallestInt(args) {
+    return Math.min(...args)
+  }
+}
