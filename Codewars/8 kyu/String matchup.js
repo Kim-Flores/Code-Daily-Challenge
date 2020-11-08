cd8cfcf3bf80f76237da6e54e2407ad3dca24002
Kim Flores <61999639+Kim-Flores@users.noreply.github.com>
2020-11-07 23:03:14 -0500

    Create String matchup.js

---
+++ Codewars/8 kyu/String matchup.js
@@ -0,0 +1,34 @@
+/*
+Instructions:
+Given two arrays of strings, return the number of times each string of the second array appears in the first array.
+
+Example
+array1 = ['abc', 'abc', 'xyz', 'cde', 'uvw']
+array2 = ['abc', 'cde', 'uap']
+How many times do the elements in array2 appear in array1?
+
+'abc' appears twice in the first array (2)
+'cde' appears only once (1)
+'uap' does not appear in the first array (0)
+Therefore, solve(array1, array2) = [2, 1, 0]
+
+Good luck!
+*/
+
+//Solution:
+function solve(a,b){
+  let arr = []
+for(let i = 0; i < b.length; i++){
+  let counter = 0;
+  for(let j =0; j < a.length; j++){
+     if(b[i] === a[j]){
+     counter++
+     }
+  }
+  arr.push(counter)
+}
+  return arr
+}
+
+//Other Solution:
+const solve = (a,b) => b.map(x => a.filter(n => n === x).length);
