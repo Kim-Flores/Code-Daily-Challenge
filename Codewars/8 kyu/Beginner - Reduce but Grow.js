5d14c9bc518bb52ac24212cfe72d17c93183214b
Kim Flores <61999639+Kim-Flores@users.noreply.github.com>
2020-11-26 23:41:53 -0500

    Create Beginner - Reduce but Grow.js

---
+++ Codewars/8 kyu/Beginner - Reduce but Grow.js
@@ -0,0 +1,23 @@
+/*
+Instructions:
+Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+
+[1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+
+*/
+
+//Solution:
+
+function grow(x){
+return x.reduce((a,c)=> a*c)
+
+}
+
+//Other Solution:
+const grow = x => {
+  let res = 1;
+  for (let i = 0; i < x.length; i++) {
+    res *= x[i];
+  }
+  return res;
+};
