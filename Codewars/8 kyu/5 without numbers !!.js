4442941c5355b9f7e69c7f5145cfd4765e241a23
Kim Flores <61999639+Kim-Flores@users.noreply.github.com>
2020-12-27 19:01:45 -0500

    Create 5 without numbers !!.js

---
+++ Codewars/8 kyu/5 without numbers !!.js
@@ -0,0 +1,16 @@
+/*
+Instructions:
+
+Write a function that always returns 5
+
+Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+Good luck :)
+*/
+
+//Solution:
+
+function unusualFive() {
+  let ok = 'hello'
+  return ok.length
+}
