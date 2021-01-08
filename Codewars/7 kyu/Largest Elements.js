0db1e4428148b957e2db5144e355eec20d7655df
Kim Flores <61999639+Kim-Flores@users.noreply.github.com>
2021-01-07 22:28:36 -0500

    Create Largest Elements.js

---
+++ Codewars/7 kyu/Largest Elements.js
@@ -0,0 +1,16 @@
+/*
+Instructions:
+Write a program that outputs the top n elements from a list.
+
+Example:
+
+largest(2, [7,6,5,4,3,2,1])
+// => [6,7]
+*/
+
+//Solution:
+function largest(n,xs){
+    // Find the n highest elements in a list
+  return xs.sort((a,b) => b - a).slice(0, n).reverse()
+}
+
