5479ec02c4d682f585c0f130272847d7db5304fb
Kim Flores <61999639+Kim-Flores@users.noreply.github.com>
2021-01-15 19:29:47 -0500

    Create JavaScript Array Filter.js

---
+++ Codewars/7 kyu/JavaScript Array Filter.js
@@ -0,0 +1,19 @@
+/*
+Instructions:
+JavaScript Arrays support a filter function (starting in JavaScript 1.6). Use the filter functionality to complete the function given.
+
+The solution would work like the following:
+
+getEvenNumbers([2,4,5,6]) // should == [2,4,6]
+*/
+
+//Solution:
+function getEvenNumbers(numbersArray){
+  return numbersArray.filter(item => item % 2 === 0)
+}
+
+//Other Solution:
+function getEvenNumbers(numbersArray){
+  return numbersArray.filter(function(num){return !(num % 2) })  // 0 is falsy
+}
+
