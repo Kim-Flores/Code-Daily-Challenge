50d4cb27aec22c472bfb519a3be24b31e233faf1
Kim Flores <61999639+Kim-Flores@users.noreply.github.com>
2021-01-20 11:00:23 -0500

    Create Broken Counter.js

---
+++ Codewars/8 kyu/Broken Counter.js
@@ -0,0 +1,21 @@
+/*
+Instructions:
+Our counter prototype is broken. Can you spot, what's wrong here?
+*/
+
+//Solution:
+function Counter() {
+  this.value = 0;
+}
+
+Counter.prototype.increase = function(){
+  this.value++;
+};
+
+Counter.prototype.getValue = function(){
+  return this.value;
+};
+
+Counter.prototype.reset = function(){
+  this.value = 0;
+};
