11261f0fb83685fc7ee90107317580981c144127
Kim Flores <61999639+Kim-Flores@users.noreply.github.com>
2021-02-18 22:59:50 -0500

    Create How do I compare numbers?.rb

---
+++ Codewars-Ruby/8 kyu/How do I compare numbers?.rb
@@ -0,0 +1,17 @@
+/*
+Instructions:
+What could be easier than comparing integer numbers? 
+However, the given piece of code doesn't recognize some of the special numbers for a reason to be found. Your task is to find the bug and eliminate it.
+*/
+
+//Solution:
+def what_is(x)
+  puts x
+  if x.equal?(42)
+    'everything'
+  elsif x == (42 ** 42)
+    'everything everythinged'
+  else
+    'nothing'
+  end
+end
