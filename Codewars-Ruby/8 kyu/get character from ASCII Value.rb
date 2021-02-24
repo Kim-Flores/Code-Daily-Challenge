e8c08618c912001fc3fbf0b80d8fbd84759288a6
Kim Flores <61999639+Kim-Flores@users.noreply.github.com>
2021-02-24 10:34:00 -0500

    Update get character from ASCII Value.rb

--- Codewars-Ruby/8 kyu/get character from ASCII Value.rb
+++ Codewars-Ruby/8 kyu/get character from ASCII Value.rb
@@ -1,13 +1,13 @@
-/*
+=begin
 Instructions:
 Write a function which takes a number and returns the corresponding ASCII char for that value.
 
 Example:
 
 get_char(65) # => 'A'
-*/
+=end
 
-//Solution:
+#Solution:
 def getChar(c)
  c.chr
 end
