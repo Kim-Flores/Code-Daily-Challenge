d3fcc50d282d23f31d0aaee8c0ec954332f288ea
Kim Flores <61999639+Kim-Flores@users.noreply.github.com>
2021-02-24 10:27:31 -0500

    Update Ensure question.rb

--- Codewars-Ruby/8 kyu/Ensure question.rb
+++ Codewars-Ruby/8 kyu/Ensure question.rb
@@ -1,17 +1,17 @@
-/*
+=begin
 Instructions:
 Given a string, write a function that returns the string with a question mark ("?") appends to the end, unless the original string ends with a question mark, in which case, returns the original string.
 
 ensure_question("Yes") # => "Yes?" 
 ensure_question("No?") # => "No?"
-*/
+=end
 
-//Solution:
+#Solution:
 def ensure_question(s)
   s.end_with?('?') == false ? s.insert(-1, "?") : s
 end
 
-//Other Solutions:
+#Other Solutions:
 def ensure_question(s)
   s.end_with?("?") ? s : s+"?"
 end
