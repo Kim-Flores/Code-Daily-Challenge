8659aadb3b1a232b2b126149c0f5b951cd12e1a7
Kim Flores <61999639+Kim-Flores@users.noreply.github.com>
2021-03-02 09:38:55 -0500

    Create  Incorrect division method.rb

---
+++ Codewars-Ruby/8 kyu/Incorrect division method.rb
@@ -0,0 +1,9 @@
+=begin
+Instructions:
+This method, which is supposed to return the result of dividing its first argument by its second, isn't always returning correct values. Fix it.
+=end
+
+#Solution:
+def divide_numbers (x, y)
+  x / y.to_f
+end
