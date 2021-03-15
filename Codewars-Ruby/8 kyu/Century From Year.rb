bcb8f002ec8f3203b0c009554b03e6197361d94b
Kim Flores <61999639+Kim-Flores@users.noreply.github.com>
2021-03-15 13:55:59 -0400

    Create Century From Year.rb

---
+++ Codewars-Ruby/8 kyu/Century From Year.rb
@@ -0,0 +1,29 @@
+=begin
+Introduction
+The first century spans from the year 1 up to and including the year 100, The second - from the year 101 up to and including the year 200, etc.
+
+Task :
+Given a year, return the century it is in.
+
+Input , Output Examples ::
+centuryFromYear(1705)  returns (18)
+centuryFromYear(1900)  returns (19)
+centuryFromYear(1601)  returns (17)
+centuryFromYear(2000)  returns (20)
+=end
+
+#Solution:
+def century(year)
+  if year >= 1 && year <= 100
+    return 1
+  elsif year % 100 != 0
+  return (year / 100).ceil + 1
+  else
+    year / 100
+  end
+end
+
+#Other Solution:
+def century(year)
+  ( year / 100.0 ).ceil
+end
