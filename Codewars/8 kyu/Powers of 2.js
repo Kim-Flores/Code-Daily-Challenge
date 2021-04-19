6afdc11988fdb43f48ad7e464316e98afb50c28b
Kim Flores <61999639+Kim-Flores@users.noreply.github.com>
2021-04-19 15:57:40 -0400

    Create  Powers of 2.js

---
+++ Codewars/8 kyu/Powers of 2.js
@@ -0,0 +1,28 @@
+/* 
+Instructions:
+Complete the function that takes a non-negative integer n as input, and returns a list of all the powers of 2 with the exponent ranging from 0 to n (inclusive).
+
+Examples
+n = 0  ==> [1]        # [2^0]
+n = 1  ==> [1, 2]     # [2^0, 2^1]
+n = 2  ==> [1, 2, 4]  # [2^0, 2^1, 2^2]
+*/
+
+//Solution:
+function powersOfTwo(n){
+  if(n == 0) return [1]
+  let range = []
+  for(let i = 0; i <= n; i++){
+    range.push(Math.pow(2,i))
+  }
+  return range
+}
+
+//Other solution:
+function powersOfTwo(n){
+  var result = [];
+  for (var i = 0; i <= n; i++) {
+    result.push(Math.pow(2, i));
+  }
+  return result;
+}
