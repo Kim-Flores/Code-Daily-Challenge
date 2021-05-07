bf1a26c597a8da50ac64a54d15a30f68d94b6678
Kim Flores <61999639+Kim-Flores@users.noreply.github.com>
2021-05-07 16:57:35 -0400

    Create  Sort arrays - 1.js

---
+++ Codewars/7 kyu/Sort arrays - 1.js
@@ -0,0 +1,6 @@
+// Instructions: Just a simple sorting usage. Create a function that returns the elements of the input-array / list in a sorted manner.
+
+//Solution:
+sortme = function( names ){
+  return names.sort()
+}
