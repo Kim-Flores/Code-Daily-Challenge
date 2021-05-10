67955d92dea2714cd5784ab71be9ca5c3904f0c7
Kim Flores <61999639+Kim-Flores@users.noreply.github.com>
2021-05-10 09:46:29 -0400

    Create Sort arrays - 1.rb

---
+++ Codewars-Ruby/7 kyu/Sort arrays - 1.rb
@@ -0,0 +1,10 @@
+=begin
+Just a simple sorting usage. Create a function that returns the elements of the input-array / list in a sorted manner.
+=end
+
+#solution:
+# input: names - unsorted array
+# output: sorted array
+def sortme( names )
+  names.sort
+end
